a303ead779d29e305eb068e25d4092f9528bfd3d
Sakshi Agarwal <devsakshiagarwal@gmail.com>
2020-06-01 13:20:27 +0400

    first commit

---
+++ app/src/main/java/com/goyal/roboads/MainActivity.java
@@ -0,0 +1,14 @@
+package com.goyal.roboads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
